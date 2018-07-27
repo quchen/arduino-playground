f9372bec6cd138d9328a3a3eedc4ecc985f9e1c9
David Luposchainsky <dluposchainsky@gmail.com>
2018-07-27 16:14:20 +0200

    quicksave

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,8 @@
+#include <fix_fft.h>
+
+#define MEASUREMENTS_EXP 7
+#define MEASUREMENTS (1 << MEASUREMENTS_EXP)
+
 const int ledPin = 3;
 const int microphonePin = A0;
 double microphoneOffset = 0;
@@ -15,18 +20,21 @@ void setup() {
     pinMode(ledPin, OUTPUT);
     pinMode(distanceTriggerPin, OUTPUT);
     pinMode(distanceEchoPin, INPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
 
-    // gaugeMicrophoneAnalog(3000);
+    gaugeMicrophoneAnalog(3000);
 
     digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
     micLoop();
+    // fourierLoop();
 }
 
-double spreadd = 0;
+double smoothMicMeasurement = 0;
+double sigmoidCenter = 10;
+double movingAverage = 0;
 void micLoop() {
     const long measureWindowMillis = 10;
     const long startTime = millis();
@@ -38,13 +46,18 @@ void micLoop() {
         maximum = max(maximum, val);
     }
     int spread = maximum - minimum;
-    spreadd = spreadd + 0.3 * ((double) spread - spreadd);
+    smoothMicMeasurement = smoothMicMeasurement + 0.3 * ((double) spread - smoothMicMeasurement);
 
-    double movingAverage;
-    movingAverage = 1023 * sigmoid(spreadd, 0.7, 15);
+    double sigmoidMovingSpread = 0;
+    movingAverage = movingAverage + 0.33 * (sigmoidMovingSpread - movingAverage);
+    sigmoidMovingSpread = 1023 * sigmoid(movingAverage, 0.95, 1.1 * sigmoidCenter);
+
+    // sigmoidMovingSpread = smoothMicMeasurement;
     Serial.print(movingAverage);
 
     analogWrite(ledPin, constrain(movingAverage, 0, 1023));
+
+    sigmoidCenter = sigmoidCenter + 0.01 * (smoothMicMeasurement - sigmoidCenter);
 
     Serial.println("");
 }
@@ -74,4 +87,33 @@ int measureMicrophoneDigital() {
 
 double sigmoid(double x, double a, double b) {
     return 1 / (1 + exp(-a * (x - b)));
+}
+
+void measureLoop() {
+    char measurements[MEASUREMENTS];
+    digitalWrite(LED_BUILTIN, HIGH);
+    for(int i = 0; i < MEASUREMENTS; ++i) {
+        measurements[i] = measureMicrophoneDigital() >> 2;
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+    // Serial.println(measurements);
+    for(int i = 0; i < MEASUREMENTS; ++i) {
+       Serial.println((int) measurements[i]);
+    }
+}
+
+void fourierLoop() {
+    char measurementsReal[MEASUREMENTS];
+    char measurementsImag[MEASUREMENTS];
+    digitalWrite(LED_BUILTIN, HIGH);
+    for(int i = 0; i < MEASUREMENTS; ++i) {
+        measurementsReal[i] = measureMicrophoneDigital() >> 2;
+        measurementsImag[i] = 0;
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // fix_fft(char fr[], char fi[], int m, int inverse);
+    fix_fft(measurementsReal, measurementsImag, MEASUREMENTS_EXP, 0);
+
+    Serial.println((int) measurementsReal[3]);
 }
